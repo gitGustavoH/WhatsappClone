88afe3a65d587b66f6e189e57d1984502c6d5625
gus <gustavohenrique.cns@gmail.com>
2024-03-16 17:03:47 -0300

    Firebase auth configurado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/example/whatsappclone/LoginActivity.kt
+++ app/src/main/java/com/example/whatsappclone/LoginActivity.kt
@@ -1,20 +1,43 @@
 package com.example.whatsappclone
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.whatsappclone.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.Cadastro.setOnClickListener {
+            registrarUsuario()
+        }
+
     }
+
+    private fun registrarUsuario() {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            "gustavo@gmail.com", "gustavo1o"
+        ).addOnSuccessListener {
+            Toast.makeText(this, "feito!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
